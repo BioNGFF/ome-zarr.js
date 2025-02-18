707d9102be240d6bd1446fb509f78e487730b447
William Moore <w.moore@dundee.ac.uk>
2025-02-18 11:50:17 +0000

    Use vite-plugin-dts to generate typescript declaration files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -26,4 +27,5 @@ export default defineConfig({
       },
     },
   },
+  plugins: [dts({ tsconfigPath: './tsconfig.json' })]
 })
