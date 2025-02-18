23d8bf11ec4e2e223d51f4e2fb783f8132cfee46
William Moore <w.moore@dundee.ac.uk>
2025-02-18 11:50:45 +0000

    Export getSlices() and getMinMaxValues()

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-
-export {getArray, getMultiscale, getMultiscaleWithArray, renderTo8bitArray} from "./utils";
-export {renderThumbnail, renderImage} from "./render";
-export {LUTS, getLuts} from "./luts";
+export {
+  getArray,
+  getMultiscale,
+  getMultiscaleWithArray,
+  renderTo8bitArray,
+  getSlices,
+  getMinMaxValues,
+} from "./utils";
+export { renderThumbnail, renderImage } from "./render";
+export { LUTS, getLuts } from "./luts";
