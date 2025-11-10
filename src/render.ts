87998dc62b1f1bbaed0e8fba542257d1b83a864a
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2025-11-10 11:49:19 -0300

    Extract out rbgData generation and DataUrl conversion

--- src/render.ts
+++ src/render.ts
@@ -92,97 +92,104 @@ export async function renderThumbnail(
 }
 
 export async function renderImage(
-    arr: zarr.Array<any>,
-    axes: Axis[],
-    omero: Omero | null | undefined,
-    sliceIndices: {[k: string]: (number | [number, number] | undefined)}  = {},
-    autoBoost: boolean = false,
-    originalShape?: number[],
-  ) {
-    // Main rendering function...
-    // We have the zarr Array already in hand, axes for dimensions
-    // and omero for rendering settings
-    // if autoBoost is true, check histogram and boost contrast if needed
-    let shape = arr.shape;
-  
-    // NB: v0.2 no axes. v0.3 is just list of 'x', 'y', 'z', 'c', 't'
-    // v0.4 onwards is list of Axis objects
-    let axesNames = axes?.map((a) => a.name || a.toString()) || ['t', 'c', 'z', 'y', 'x'];
-    let chDim = axesNames.indexOf("c");
-    let channel_count = shape[chDim] || 1;
-    let visibilities;
-    // list of [r,g,b] colors
-    let rgbColors: Array<[number, number, number]>;
-    let luts: Array<string | undefined> | undefined = undefined;
-    let inverteds: Array<boolean> | undefined = undefined;
-  
-    // If we have 'omero', use it for channel rgbColors and visibilities
-    if (omero) {
-      let active_count = 0;
-      visibilities = omero.channels.map((ch) => {
-        if (ch.active == undefined) {
-          ch.active = true;
-        }
-        active_count += ch.active ? 1 : 0;
-        return ch.active && active_count <= MAX_CHANNELS;
-      });
-      rgbColors = omero.channels.map((ch) => hexToRGB(ch.color));
-      luts = omero.channels.map((ch) => "lut" in ch ? ch.lut as string : undefined);
-    } else {
-      visibilities = getDefaultVisibilities(channel_count);
-      rgbColors = getDefaultColors(channel_count, visibilities);
-    }
-    // filter for active channels
-    let activeChannelIndices = visibilities.reduce((prev, active, index) => {
-      if (active) prev.push(index);
-      return prev;
-    }, []);
-    rgbColors = activeChannelIndices.map((chIndex: number) => rgbColors[chIndex]);
-    inverteds = activeChannelIndices.map((chIndex: number) => Boolean(omero?.channels[chIndex].inverted));
-    if (luts !== undefined) {
-      luts = luts.filter((_, index) => activeChannelIndices.includes(index));
-    }
-  
-    // Get slices for each channel
-    if (sliceIndices["z"] == undefined) {
-        sliceIndices["z"] = omero?.rdefs?.defaultZ;
-    }
-    if (sliceIndices["t"] == undefined) {
-      sliceIndices["t"] = omero?.rdefs?.defaultT;
-    }
-    // sliceIndices are from originalShape if provided
-    let chSlices = getSlices(activeChannelIndices, shape, axesNames, sliceIndices, originalShape);
-  
-    // Wait for all chunks to be fetched...
-    let promises = chSlices.map((chSlice: any) => zarr.get(arr, chSlice));
-    let ndChunks = await Promise.all(promises);
-  
-    // Use start/end values from 'omero' if available, otherwise calculate min/max
-    let minMaxValues = activeChannelIndices.map((chIndex:number, i:number) => {
-        if (omero && omero.channels[chIndex]) {
-            let chOmero = omero.channels[chIndex];
-            if (chOmero?.window?.start !== undefined && chOmero?.window?.end !== undefined) {
-                return [chOmero.window.start, chOmero.window.end];
-            }
-        }
-        return getMinMaxValues(ndChunks[i]);
-    });
+  arr: zarr.Array<any>,
+  axes: Axis[],
+  omero: Omero | null | undefined,
+  sliceIndices: { [k: string]: (number | [number, number] | undefined) } = {},
+  autoBoost: boolean = false,
+  originalShape?: number[],
+) {
+  // Main rendering function...
+  // We have the zarr Array already in hand, axes for dimensions
+  // and omero for rendering settings
+  // if autoBoost is true, check histogram and boost contrast if needed
+  let { ndChunks, rbgData } = await getChunksAndRbgData(arr, axes, omero, sliceIndices, originalShape, autoBoost);
 
-    // Render to 8bit rgb array
-    let rbgData = renderTo8bitArray(ndChunks, minMaxValues, rgbColors, luts, inverteds, autoBoost);
-    // Use a canvas element to convert the 8bit array to a dataUrl
-    const canvas = document.createElement("canvas");
-    const height = ndChunks[0].shape[0];
-    const width = ndChunks[0].shape[1];
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext("2d");
-    if (!context) {
-      return "";
-    }
-    context.putImageData(new ImageData(rbgData, width, height), 0, 0);
-    let dataUrl = canvas.toDataURL("image/png");
-    return dataUrl;
+  const height = ndChunks[0].shape[0];
+  const width = ndChunks[0].shape[1];
+
+  return convertRbgDataToDataUrl(rbgData, width, height);
+}
+
+async function getChunksAndRbgData(arr: zarr.Array<any, zarr.Readable>, axes: Axis[], omero: Omero | null | undefined, sliceIndices: { [k: string]: number | [number, number] | undefined; }, originalShape: number[] | undefined, autoBoost: boolean) {
+  let shape = arr.shape;
+
+  // NB: v0.2 no axes. v0.3 is just list of 'x', 'y', 'z', 'c', 't'
+  // v0.4 onwards is list of Axis objects
+  let axesNames = axes?.map((a) => a.name || a.toString()) || ['t', 'c', 'z', 'y', 'x'];
+  let chDim = axesNames.indexOf("c");
+  let channel_count = shape[chDim] || 1;
+  let visibilities;
+  // list of [r,g,b] colors
+  let rgbColors: Array<[number, number, number]>;
+  let luts: Array<string | undefined> | undefined = undefined;
+  let inverteds: Array<boolean> | undefined = undefined;
+
+  // If we have 'omero', use it for channel rgbColors and visibilities
+  if (omero) {
+    let active_count = 0;
+    visibilities = omero.channels.map((ch) => {
+      if (ch.active == undefined) {
+        ch.active = true;
+      }
+      active_count += ch.active ? 1 : 0;
+      return ch.active && active_count <= MAX_CHANNELS;
+    });
+    rgbColors = omero.channels.map((ch) => hexToRGB(ch.color));
+    luts = omero.channels.map((ch) => "lut" in ch ? ch.lut as string : undefined);
+  } else {
+    visibilities = getDefaultVisibilities(channel_count);
+    rgbColors = getDefaultColors(channel_count, visibilities);
+  }
+  // filter for active channels
+  let activeChannelIndices = visibilities.reduce((prev, active, index) => {
+    if (active) prev.push(index);
+    return prev;
+  }, []);
+  rgbColors = activeChannelIndices.map((chIndex: number) => rgbColors[chIndex]);
+  inverteds = activeChannelIndices.map((chIndex: number) => Boolean(omero?.channels[chIndex].inverted));
+  if (luts !== undefined) {
+    luts = luts.filter((_, index) => activeChannelIndices.includes(index));
   }
 
-  
+  // Get slices for each channel
+  if (sliceIndices["z"] == undefined) {
+    sliceIndices["z"] = omero?.rdefs?.defaultZ;
+  }
+  if (sliceIndices["t"] == undefined) {
+    sliceIndices["t"] = omero?.rdefs?.defaultT;
+  }
+  // sliceIndices are from originalShape if provided
+  let chSlices = getSlices(activeChannelIndices, shape, axesNames, sliceIndices, originalShape);
+
+  // Wait for all chunks to be fetched...
+  let promises = chSlices.map((chSlice: any) => zarr.get(arr, chSlice));
+  let ndChunks = await Promise.all(promises);
+
+  // Use start/end values from 'omero' if available, otherwise calculate min/max
+  let minMaxValues = activeChannelIndices.map((chIndex: number, i: number) => {
+    if (omero && omero.channels[chIndex]) {
+      let chOmero = omero.channels[chIndex];
+      if (chOmero?.window?.start !== undefined && chOmero?.window?.end !== undefined) {
+        return [chOmero.window.start, chOmero.window.end];
+      }
+    }
+    return getMinMaxValues(ndChunks[i]);
+  });
+
+  // Render to 8bit rgb array
+  let rbgData = renderTo8bitArray(ndChunks, minMaxValues, rgbColors, luts, inverteds, autoBoost);
+  return { ndChunks, rbgData };
+}
+
+function convertRbgDataToDataUrl(rbgData: Uint8ClampedArray, width: number, height: number): string {
+  const canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  const context = canvas.getContext("2d");
+  if (!context) {
+    return "";
+  }
+  context.putImageData(new ImageData(rbgData, width, height), 0, 0);
+  return canvas.toDataURL("image/png");
+}
