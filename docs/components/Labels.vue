eaa117a746150aac2bf0216aa9bf1be41852b91a
William Moore <w.moore@dundee.ac.uk>
2026-03-30 17:38:25 +0100

    Fixing and tidy of labels docs page

--- docs/components/Labels.vue
+++ docs/components/Labels.vue
@@ -1,50 +1,110 @@
 <script setup>
-
-import { onMounted } from 'vue';
-import { ref } from 'vue';
+import { onMounted } from "vue";
+import { ref } from "vue";
 
 const imgSrc = ref("");
 const labelSrc = ref("");
 
-const VURL = "https://ome.github.io/ome-ngff-validator/?source="
+const VURL = "https://ome.github.io/ome-ngff-validator/?source=";
 
 onMounted(async () => {
-
   // This loads from http://localhost:5173/ome-zarr.js/@fs/Users/wmoore/Desktop/ZARR/ome-zarr.js/dist/ome-zarr.js
   // NB: needs `npm run build` first!
-  const omezarr = await import('ome-zarr.js');
+  const omezarr = await import("ome-zarr.js");
 
   function blackToTransparentRgba(d) {
     for (let i = 0; i < d.length; i += 4) {
       if (d[i] === 0 && d[i + 1] === 0 && d[i + 2] === 0) {
-      d[i + 3] = 0;
+        d[i + 3] = 0;
       }
     }
   }
 
-  let url = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/2/";
+  let url =
+    "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/2/";
   let img = await omezarr.NgffImage.load(url);
-  imgSrc.value = await img.render({targetSize: 300})
+  imgSrc.value = await img.render({ targetSize: 300 });
 
   let labelPaths = await img.getLabelsPaths();
-  let labelImage = await omezarr.NgffImage.load(url + "labels/" + labelPaths[0]);
+  let labelImage = await omezarr.NgffImage.load(
+    url + "labels/" + labelPaths[0]
+  );
   labelImage.setChannelActive(0, true);
   labelImage.setChannelLut(0, "glasbey_inverted.lut");
 
   // renderRgba gives us an rgba array we can manipulate, to convert black to transparent
-  let labelRgba = await labelImage.renderRgba({targetSize: 300});
+  let labelRgba = await labelImage.renderRgba({ targetSize: 300 });
   blackToTransparentRgba(labelRgba.data);
 
-
   // convert the rgba array back to image src
-  labelSrc.value = await omezarr.convertRbgDataToDataUrl(labelRgba.data, labelRgba.width);
+  labelSrc.value = await omezarr.convertRbgDataToDataUrl(
+    labelRgba.data,
+    labelRgba.width
+  );
 });
 </script>
 
 <template>
-  <a :href="VURL + `https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/2/`" target="_blank">
+  <a
+    :href="
+      VURL +
+      `https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/2/`
+    "
+    target="_blank"
+  >
     <img alt="thumbnail" :src="imgSrc" />
-    <img alt="label" :src="labelSrc" />
+    <img :class="$style.renderedImage" alt="label" :src="labelSrc" />
   </a>
-  <h1>WTF</h1>
+
+  <p>
+    Overlay both the images above:
+  </p>
+
+  <div :class="$style.overlay">
+    <img alt="thumbnail" :src="imgSrc" />
+    <img :class="$style.fadeInOut" alt="label" :src="labelSrc" />
+  </div>
 </template>
+
+<style module>
+img {
+  margin: 10px 0;
+}
+.renderedImage {
+  /* background pattern for transparency */
+  background:
+    linear-gradient(45deg, #ccc 25%, transparent 25%),
+    linear-gradient(-45deg, #ccc 25%, transparent 25%),
+    linear-gradient(45deg, transparent 75%, #ccc 75%),
+    linear-gradient(-45deg, transparent 75%, #ccc 75%);
+  background-size: 20px 20px;
+  background-position:
+    0 0,
+    0 10px,
+    10px -10px,
+    -10px 0px;
+}
+
+.overlay {
+  position: relative;
+}
+.overlay :nth-child(2) {
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-top: 0;
+}
+
+.fadeInOut {
+  animation: fadeInOut 3s ease-in-out infinite;
+}
+@keyframes fadeInOut {
+  0%,
+  100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.1;
+  }
+}
+</style>
--- docs/labels.md
+++ docs/labels.md
@@ -5,6 +5,9 @@ We can use `ngffImage.getLabelsPaths()` to list any labels groups below a multis
 
 These can then be opened as images and rendered as normal.
 
+Alternatively, `ngffImage.renderRgba()` returns `rgba` values in an `Uint8ClampedArray`,
+which we can then manipulate as shown below:
+
 ```js
 
 function blackToTransparentRgba(d) {
@@ -15,9 +18,17 @@ function blackToTransparentRgba(d) {
     }
 }
 
+// load and render the parent image
 let url = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0079A/idr0079_images.zarr/2/";
 let img = await omezarr.NgffImage.load(url);
-let imgSrc = await img.render({targetSize: 300, autoBoost: true})
+let imgSrc = await img.render({targetSize: 300});
+document.getElementById("img").src = labelSrc;
+
+// find labels and open the first label image
+let labelPaths = await img.getLabelsPaths();
+let labelImage = await omezarr.NgffImage.load(url + "labels/" + labelPaths[0]);
+// Background will be rendered black
+labelImage.setChannelLut(0, "glasbey_inverted.lut");
 
 // renderRgba gives us an rgba array we can manipulate, to convert black to transparent
 let {data, width} = await img.renderRgba({targetSize: 300);
@@ -25,20 +36,15 @@ blackToTransparentRgba(data)
 
 // convert the rgba array back to image src
 let labelSrc = await omezarr.convertRbgDataToDataUrl(data, width);
-document.getElementById("labelImg").src += labelSrc;
+document.getElementById("labelImg").src = labelSrc;
 ```
 
 
 <script setup>
 import Labels from './components/Labels.vue';
-import Thumbnail from './components/Thumbnail.vue';
 </script>
 
 
 <ClientOnly>
-<!-- <Labels> -->
-
-<div>Test</div>
-
 <Labels />
 </ClientOnly>
