8e3745c1bf77a22106d6000c4c87f9c4b5140f7c
William Moore <w.moore@dundee.ac.uk>
2026-06-16 21:37:02 +0100

    add more tests

--- tests/renderArray.node.test.js
+++ tests/renderArray.node.test.js
@@ -1,6 +1,33 @@
 
 import { describe, expect, test } from "vitest";
-import { renderChunk, renderChunkWithLUT } from "../src/render.ts";
+import {
+    renderChunk,
+    renderChunkWithLUT,
+    renderChunkWithColormap,
+    renderChunks } from "../src/render.ts";
+import { FILL_VALUE_KEY } from "../src/utils.ts";
+
+
+test("renderChunks", async () => {
+
+    let fakeChunk = {shape: [2, 3], data: [0, 1, 2, 3, 4, 5]};
+    let colors = [[255, 0, 0], [0, 255, 0], [0, 0, 255]];
+    let rendered = renderChunks(
+        [fakeChunk, fakeChunk, fakeChunk],
+        [[0, 3], [0, 6], [0, 9]], // ranges
+        colors
+    );
+
+    let expected = new Uint8ClampedArray([
+        0, 0, 0, 255,
+       85, 43, 28, 255,
+      170, 85, 57, 255,
+      255, 128, 85, 255,
+      255, 170, 113, 255,
+      255, 213, 142, 255,
+    ]);
+    expect(rendered).toStrictEqual(expected);
+});
 
 
 test("renderChunk", async () => {
@@ -20,7 +47,7 @@ test("renderChunk", async () => {
 
 
 test("renderChunkWithLUT", async () => {
-    
+
     let fakeChunk = {shape: [3, 3], data: [0, 1, 2, 3, 4, 5, 6, 7, 8]};
     let transparent = [0, 0, 0, 0];
     let red = [255, 0, 0];
@@ -53,7 +80,6 @@ test("renderChunkWithLUTrange", async () => {
 
     // with range, values are scaled to the LUT range
     let rendered = renderChunkWithLUT(fakeChunk, lut, {range: [0, 8]});
-    console.log("rendered", rendered);
 
     let expected = new Uint8ClampedArray([
         0,    0,   0, 255,
@@ -69,3 +95,36 @@ test("renderChunkWithLUTrange", async () => {
     expect(rendered).toStrictEqual(expected);
 });
 
+
+test("renderChunkWithColormap", async () => {
+
+    let fakeChunk = {shape: [2, 3], data: [0, 1, 2, 3, 4, 5]};
+    let colorMap = new Map();
+    colorMap.set(0, [0, 0, 0, 255]);
+    colorMap.set(1, [255, 0, 0, 255]);
+    colorMap.set(2, [0, 255, 0, 255]);
+
+    // default fill value is [0, 0, 0, 0]
+    let rendered = renderChunkWithColormap(fakeChunk, colorMap);
+    let expected = new Uint8ClampedArray([
+        0,    0,   0, 255,
+       255,   0,   0, 255,
+         0, 255,   0, 255,
+         0,   0,   0,   0,  // default fill value
+         0,   0,   0,   0,
+         0,   0,   0,   0,
+    ]);
+    expect(rendered).toStrictEqual(expected);
+
+    // custom fill value
+    rendered = renderChunkWithColormap(fakeChunk, colorMap, {fillValue: [255, 255, 255, 255]});
+    expected = new Uint8ClampedArray([
+        0,    0,   0, 255,
+       255,   0,   0, 255,
+         0, 255,   0, 255,
+         255, 255, 255, 255,  // new fill value
+         255, 255, 255, 255,
+         255, 255, 255, 255,
+    ]);
+    expect(rendered).toStrictEqual(expected);
+});
